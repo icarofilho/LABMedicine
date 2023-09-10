e7dc74716e4823aa454020e4111ba21fe7a35d54
icarofilho <icaro.ferreira.filho@gmail.com>
2023-09-10 18:19:37 -0300

    criado campos do formulário

--- src/components/AddNewPatientComponent/index.jsx
+++ src/components/AddNewPatientComponent/index.jsx
@@ -1,3 +1,114 @@
+import { Box, Grid, Button } from "@mui/material";
+import * as styled from "./styles";
+import Axios from "../../requests";
+import { useState } from "react";
 export function AddNewPatientComponent() {
-  return <h1>AddNewPatientComponent</h1>;
+  const [address, setAddress] = useState({ street: "" });
+  async function getAddress() {
+    const data = await Axios.viaCep(21931160);
+    console.log(data);
+    setAddress(data);
+  }
+  return (
+    <form>
+      <Grid container>
+        <Grid item xs={12}>
+          <styled.Input type="text" placeholder="Nome completo" />
+        </Grid>
+        <select defaultValue="Genero">
+          <option value="Genero" disabled>
+            Genero
+          </option>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+          <option value="transgenero">Transgenero</option>
+          <option value="outro">Outro</option>
+        </select>
+        <styled.Input type="date" />
+        <styled.Input type="number" />
+        <styled.Input placeholder="cpf" type="text" />
+        <styled.Input placeholder="rg" type="text" />
+        <styled.Input type="text" placeholder="Órgão" />
+        <select defaultValue={"Estado Civil"}>
+          <option value="estado civil" disabled>
+            Estado Civil
+          </option>
+          <option value="solteiro">Solteiro</option>
+          <option value="casado">Casado</option>
+          <option value="separado">Separado</option>
+          <option value="divorciado">Divorciado</option>
+          <option value="viuvo">Viúvo</option>
+        </select>
+        <styled.Input type="number" placeholder="(99) 9 9999-9999" />
+        <styled.Input type="email" />
+        <styled.Input type="number" placeholder="(99) 9 9999-9999" />
+        <textarea
+          type="text"
+          placeholder="Alergias devem ser separadas por virgula"
+        />
+        <Grid item xs={12}>
+          <styled.Input type="number" placeholder="99888777" />
+          <styled.Input type="button" onClick={getAddress} value="buscar" />
+          <styled.Input
+            type="text"
+            placeholder={address.street || "Logradouro"}
+            readOnly
+            disabled
+          />
+          <styled.Input type="text" placeholder="Número" />
+          <styled.Input
+            type="text"
+            placeholder="Complemento"
+            title="Caso não haja um complemento deixe este campo em branco"
+          />
+          <styled.Input
+            type="text"
+            placeholder={address.neighborhood || "Bairro"}
+            readOnly
+            disabled
+          />
+          <styled.Input
+            type="text"
+            placeholder={address.city || "Cidade"}
+            readOnly
+            disabled
+          />
+          <styled.Input
+            type="text"
+            placeholder={address.state || "Estado"}
+            readOnly
+            disabled
+          />
+        </Grid>
+        <Grid container>
+          <Grid item xs={6}></Grid>
+          <Grid
+            item
+            xs={6}
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "end",
+            }}
+          >
+            <Button variant="contained" color="info">
+              Cadastrar
+            </Button>
+            <Button
+              variant="outlined"
+              disabled
+              sx={{
+                margin: "0 10px",
+              }}
+            >
+              Editar
+            </Button>
+            <styled.Button variant="outlined" disabled>
+              Deletar
+            </styled.Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </form>
+  );
 }
---
+++ src/components/AddNewPatientComponent/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Button as ButtonMui } from "@mui/material";
+
+export const Button = styled(ButtonMui)``;
+
+export const Input = styled.input`
+  margin: 4px 4px;
+  height: 25px;
+`;
